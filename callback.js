03b3f87341496408d5398c37badc506ccc57fa27
coalex <coalex@MEN319424.minedu.gov.co>
2023-04-11 18:31:26 -0500

    add funtion

--- callback.js
+++ callback.js
@@ -1,3 +1,23 @@
-function soyasincrona() {
+function hola(nombre, micallback) {
     console.log("Hola soy una aplicacion asincrona")
+    setTimeout(function () {
+
+        console.log("Hola" + nombre);
+        micallback()
+    }, 1000);
 }
+
+function adios(nombre, otrocallback) {
+    setTimeout(function () {
+        console.log("Adios" + nombre)
+        otrocallback()
+
+    },1000)
+}
+
+
+console.log("Iniciando proceco")
+hola('John', function name() {
+
+    console.log("Terminando Proceco")
+})
