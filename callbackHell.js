5dea80c4fddbbf9a63ab84b53d5d40ac377ec53a
coalex <coalex@MEN319424.minedu.gov.co>
2023-04-11 18:37:35 -0500

    add callbackhablar

---
+++ callbackHell.js
@@ -0,0 +1,29 @@
+function hola(nombre, micallback) {
+    console.log("Hola soy una aplicacion asincrona")
+    setTimeout(function () {
+
+        console.log("Hola" + nombre);
+        micallback()
+    }, 1000);
+}
+function hablar(callbackHablar) {
+    setTimeout(function () {
+        console.log("Bla bla bla bla...")
+        callbackHablar()
+    }, 1000)
+}
+
+function adios(nombre, otrocallback) {
+    setTimeout(function () {
+        console.log("Adios" + nombre)
+        otrocallback()
+
+    }, 1000)
+}
+
+
+console.log("Iniciando proceco")
+hola('John', function name() {
+
+    console.log("Terminando Proceco")
+})
