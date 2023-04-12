4b25a2b88fdcce2952544314f0a6a7ff2e6b0018
john Cotamo <johncotamo@hotmail.com>
2023-04-11 22:24:01 -0500

    add to asycnAway

--- promises.js
+++ promises.js
@@ -1,36 +1,31 @@
 function hola(nombre, micallback) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-
-            console.log("Hola" + nombre);
-            resolve()
-        }, 1000);
-
-    });
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      console.log("Hola" + nombre);
+      resolve();
+    }, 1000);
+  });
 }
 function hablar(callbackHablar) {
-    setTimeout(function () {
-        console.log("Bla bla bla bla...")
-        callbackHablar()
-    }, 1000)
+  setTimeout(function () {
+    console.log("Bla bla bla bla...");
+    callbackHablar();
+  }, 1000);
 }
 
 function adios(nombre) {
-    return new Promise((resolve, reject) => {
-
-        setTimeout(function () {
-            console.log("Adios" + nombre)
-            resolve()
-
-        }, 1000)
-    })
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      console.log("Adios" + nombre);
+      resolve();
+    }, 1000);
+  });
 }
-console.log("Iniciando el proceso")
+console.log("Iniciando el proceso");
 
 hola("john")
-    .then(hablar)
-    .then(adios)
-    .then((nombre) => {
-        console.log("Terminado el proceso")
-
-    })
+  .then(hablar)
+  .then(adios)
+  .then((nombre) => {
+    console.log("Terminado el proceso");
+  });
