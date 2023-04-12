11beab77ef8236639d0e1a6dd1ba46c66143a50a
john Cotamo <johncotamo@hotmail.com>
2023-04-11 22:53:00 -0500

    add to async to funciones

--- asyncAway.js
+++ asyncAway.js
@@ -1,16 +1,18 @@
-async function hola(nombre, micallback) {
+// La palabra async  la convierte inmediatamente en asíncrona.
+async function hola(nombre) {
   return new Promise(function (resolve, reject) {
     setTimeout(function () {
-      console.log("Hola" + nombre);
-      resolve();
-    }, 1000);
+      console.log("Hola, " + nombre);
+      resolve(nombre);
+    }, 1500);
   });
 }
+
 async function hablar(nombre) {
-  return new Promise((resolve, rejec) => {
+  return new Promise((resolve, reject) => {
     setTimeout(function () {
       console.log("Bla bla bla bla...");
-      rejec();
+      resolve("Hay un error");
     }, 1000);
   });
 }
@@ -18,22 +20,23 @@ async function hablar(nombre) {
 async function adios(nombre) {
   return new Promise((resolve, reject) => {
     setTimeout(function () {
-      console.log("Adios" + nombre);
+      console.log("Adios", nombre);
       resolve();
     }, 1000);
   });
 }
-console.log("Iniciando el proceso");
 
-hola("john")
-  .then(hablar)
-  .then(adios)
-  .then((nombre) => {
-    console.log("Terminado el proceso");
-  });
-
+// Await solo es válido dentro de una función asíncrona.
 async function main() {
-  await hola("Cotamo");
+  let nombre = await hola("Alejandro");
+  await hablar();
+  hablar(); // Para hacer que se ejecute en segundo plano no debe existi el await
+  await hablar();
+  await adios(nombre);
+  console.log("Termina el proceso");
 }
 
+// Esto nos permitirá saber si nuestra función se está ejecutanod de forma asíncrona.
+console.log("Empezamos el proceso");
 main();
+console.log("Va a ser la segunda instrucción");
