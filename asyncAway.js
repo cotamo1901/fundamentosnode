61e376f3b78373d6f8afa9eb3b1c3d7a897ddb4b
john Cotamo <johncotamo@hotmail.com>
2023-04-11 22:48:30 -0500

    add to async to funciones

--- asyncAway.js
+++ asyncAway.js
@@ -1,32 +1,39 @@
-function hola(nombre, micallback) {
-    return new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        console.log("Hola" + nombre);
-        resolve();
-      }, 1000);
-    });
-  }
-  function hablar(callbackHablar) {
+async function hola(nombre, micallback) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      console.log("Hola" + nombre);
+      resolve();
+    }, 1000);
+  });
+}
+async function hablar(nombre) {
+  return new Promise((resolve, rejec) => {
     setTimeout(function () {
       console.log("Bla bla bla bla...");
-      callbackHablar();
+      rejec();
     }, 1000);
-  }
-  
-  function adios(nombre) {
-    return new Promise((resolve, reject) => {
-      setTimeout(function () {
-        console.log("Adios" + nombre);
-        resolve();
-      }, 1000);
-    });
-  }
-  console.log("Iniciando el proceso");
-  
-  hola("john")
-    .then(hablar)
-    .then(adios)
-    .then((nombre) => {
-      console.log("Terminado el proceso");
-    });
-  
+  });
+}
+
+async function adios(nombre) {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      console.log("Adios" + nombre);
+      resolve();
+    }, 1000);
+  });
+}
+console.log("Iniciando el proceso");
+
+hola("john")
+  .then(hablar)
+  .then(adios)
+  .then((nombre) => {
+    console.log("Terminado el proceso");
+  });
+
+async function main() {
+  await hola("Cotamo");
+}
+
+main();
