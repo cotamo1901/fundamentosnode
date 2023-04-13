b7fa5327cf5534088d602676cabf79f6c471714b
john Cotamo <johncotamo@hotmail.com>
2023-04-13 18:36:54 -0500

    dl exec muestra procesos de linux en terminal ejemplo ls -la

---
+++ modulos/procesoHijo.js
@@ -0,0 +1,14 @@
+const { exec } = require('child_process')
+
+
+exec('ls -la', (err, stdout, sterr) => {
+    if (err) {
+        console.error(err);
+        return false
+
+    }
+    console.log(stdout
+
+
+    )
+})
