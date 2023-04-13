af1ff37651f238cd99e0106313bbe1407e6e527b
john Cotamo <johncotamo@hotmail.com>
2023-04-12 22:20:59 -0500

    errores

---
+++ modulos/consola.js
@@ -0,0 +1,19 @@
+console.log("Algo");
+console.info("Lo mismo");
+console.error("Es un error");
+console.warn("Esto es una alerta");
+console.count("averces")
+var table = [
+  {
+    a: 1,
+    b: 2,
+    c: 3,
+  },
+];
+console.table(table);
+
+console.group(table);
+
+function function1() {
+
+}
